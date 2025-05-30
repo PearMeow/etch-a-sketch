64db13a9b8c48bf6dc6b2571a4beca544517be1a
pearmeow <perryhuang2015@gmail.com>
2025-05-30 15:15:14 -0400

    Automatically make grid squares with js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 console.log("If you can see this, the script is loaded");
+
+const theContainer = document.querySelector(".container");
+const gridWidth = 16;
+const gridHeight = 16;
+
+let grid = [];
+
+for (let i = 0; i < gridWidth; ++i) {
+    grid.push([]);
+    for (let j = 0; j < gridWidth; ++j) {
+        let square = document.createElement("div");
+        grid[i] = square;
+        grid[i].className = "square";
+        grid[i].textContent = `${i}, ${j}`;
+        theContainer.appendChild(grid[i]);
+    }
+}
