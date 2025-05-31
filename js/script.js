3f3b76486aac608f0209f98e8730b07205f608ee
pearmeow <perryhuang2015@gmail.com>
2025-05-31 12:22:02 -0400

    Modify css so grid is in fixed space

--- js/script.js
+++ js/script.js
@@ -7,15 +7,33 @@ const gridHeight = 16;
 let grid = [];
 let rowContainers = [];
 
-for (let i = 0; i < gridWidth; ++i) {
-    let rowCont = document.createElement("div");
-    rowCont.className = "rowContainer";
-    rowContainers.push(rowCont);
-    grid.push([]);
-    for (let j = 0; j < gridHeight; ++j) {
-        grid[i].push(document.createElement("div"));
-        grid[i][j].className = "square";
-        rowCont.appendChild(grid[i][j]);
+function genGrid(gridWidth = 16, gridHeight = 16) {
+    deleteGrid();
+    for (let i = 0; i < gridWidth; ++i) {
+        let rowCont = document.createElement("div");
+        rowCont.className = "rowContainer";
+        rowContainers.push(rowCont);
+        grid.push([]);
+        for (let j = 0; j < gridHeight; ++j) {
+            grid[i].push(document.createElement("div"));
+            grid[i][j].className = "square";
+            rowCont.appendChild(grid[i][j]);
+        }
+        theContainer.appendChild(rowContainers[i]);
     }
-    theContainer.appendChild(rowContainers[i]);
 }
+
+function deleteGrid() {
+    for (let i = 0; i < grid.length; ++i) {
+        for (let j = 0; j < grid[i].length; ++j) {
+            grid[i][j].remove();
+        }
+    }
+    for (let i = 0; i < rowContainers.length; ++i) {
+        rowContainers[i].remove();
+    }
+    grid.length = 0;
+    rowContainers.length = 0;
+}
+
+genGrid();
